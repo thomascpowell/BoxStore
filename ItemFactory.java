166f3961a063dcc9f052f8b3454e200e8a2e87df
Thomas Powell <thomascoxpowell@gmail.com>
2025-03-14 17:47:30 -0400

    updated todo to reflect changes

--- ItemFactory.java
+++ ItemFactory.java
@@ -18,11 +18,9 @@ public class ItemFactory {
     System.out.println(test);
   }
 
-
   private static Item createItemHelper(String type) throws Exception {
     Scanner in = new Scanner(System.in);
     Item res;
-
     String name;
     double price;
     String expiration;
@@ -40,72 +38,61 @@ public class ItemFactory {
     boolean green;
     String brand;
     String description;
-
     name = Utils.getString(in, "Name: ");
     price = Utils.getInt(in, "Price: ");
     brand = Utils.getString(in, "Brand: ");
     description = Utils.getString(in, "Description: ");
 
     switch (type) {
-
       case "Fruit":
         citrus =  Utils.getBool(in, "Is the item citrus?");
         expiration = Utils.getString(in, "Expiration: ");
         res = new Fruit(name, price, brand, description, expiration, citrus);
         break;
-      
       case "Furniture":
         fragile = Utils.getBool(in, "Is the item fragile?");
         material = Utils.getString(in, "Material: ");
         legs = Utils.getInt(in, "Legs: ");
         res = new Furniture(name, price, brand, description, fragile, material, legs);
         break;
-
       case "Laptop":
         fragile = Utils.getBool(in, "Is the item fragile?");
         processor = Utils.getString(in, "Processor: ");
         releaseYear = Utils.getInt(in, "Release Year: ");
         res = new Laptop(name, price, brand, description, fragile, releaseYear, processor);
         break;
-
       case "Phone":
         fragile = Utils.getBool(in, "Is the item fragile?");
         formFactor = Utils.getString(in, "Form Factor: ");
         releaseYear = Utils.getInt(in, "Release Year: ");
         res = new Phone(name, price, brand, description, fragile, releaseYear, formFactor);
         break;
-
       case "Shirt":
         fragile = Utils.getBool(in, "Is the item fragile?");
         size = Utils.getInt(in, "Size: ");
         color = Utils.getString(in, "Color: ");
         res = new Shirt(name, price, brand, description, fragile, size, color);
         break;
-
       case "Shoe":
         fragile = Utils.getBool(in, "Is the item fragile?");
         size = Utils.getInt(in, "Size: ");
         hightop = Utils.getBool(in, "Is the shoe a hightop?");
         res = new Shoe(name, price, brand, description, fragile, size, hightop);
         break;
-
       case "TV":
         fragile = Utils.getBool(in, "Is the item fragile?");
         resolution = Utils.getString(in, "Resolution: ");
         releaseYear = Utils.getInt(in, "Release Year: ");
         res = new TV(name, price, brand, description, fragile, releaseYear, resolution);
         break;
-
       case "Vegetable":
         green = Utils.getBool(in, "Is the vegetable green?");
         expiration = Utils.getString(in, "Expiration: ");
         res = new Vegetable(name, price, brand, description, expiration, green);
         break;
-
       default:
         throw new Exception("Unknown Item Type");
     }
     return res;
-
   }
 }
--- README.md
+++ README.md
@@ -1,20 +1,9 @@
 Todo:
 
-1. Implement 3 unfinished functions
-    - sellItem
-    - (WIP) printTable
-    - (Complete) printOrderSummary
+1. sellItem()
 
-2. Document functions
+2. Table format for printTable()
 
-3. Charts
+3. Documentation
 
 
-Complete:
-
-1. Deal with this part:
-    "Displays available options in a table format for the selected category, 
-    including details such as the item name, price, brand (if applicable), 
-    description, and return policy."
-    - Need new fields in Item (String brand, String description)
-    - Update constructors
