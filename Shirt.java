d6760a71be51935647853df1959cfdde7b60ab3b
Thomas Powell <thomascoxpowell@gmail.com>
2025-03-24 11:50:23 -0400

    commented shirt

--- Shirt.java
+++ Shirt.java
@@ -2,19 +2,41 @@ public class Shirt extends Outerwear {
 
   private String color;
 
+  /**
+   * Creates an instance of Shoe.
+   * @param name
+   * @param price
+   * @param brand
+   * @param description
+   * @param fragile
+   * @param size
+   * @param color
+   * */
   public Shirt(String name, double price, String brand, String description, boolean fragile, int size, String color) {
     super(name, price, brand, description, fragile, size);
     this.color = color;
   }
 
+  /**
+   * Returns color.
+   * @return color
+   * */
   public String getColor() {
     return color;
   }
 
+  /**
+   * Sets the color.
+   * @param color The new color value.
+   * */
   public void setColor(String color) {
     this.color = color;
   }
 
+  /**   
+   * Overrides the toString method.
+   * @return The string representation.
+   * */
   public String toString() {
     return String.format("Quantity: %d; Name: %s; Price: %.2f; Fragile: %b; Size: %d; Color: %s",
     this.getQuantity(), this.getName(), this.getPrice(), this.isFragile(), this.getSize(), this.getColor());
